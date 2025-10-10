ea703c2f02c7f5d5294c594d0453842593772b45
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2025-10-10 14:45:41 -0300

    validator is done

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: zimbo <zimbo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 14:47:41 by zalberti          #+#    #+#             */
-/*   Updated: 2025/10/10 13:29:13 by zimbo            ###   ########.fr       */
+/*   Updated: 2025/10/10 14:44:48 by zimbo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 int main(int argc, char **argv)
 {
-	
 	ft_empty(argc);
 	ft_check_int(argc, argv);
 	ft_duplicate(argc, argv);
